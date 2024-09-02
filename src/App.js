753e608b4010cadc6a618bc55f8ef790abc852f5
hyenseochoe <whahoo1@naver.com>
2024-09-02 23:02:05 +0900

    11:01

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  let [Text, setText] = useState("");
-  let [List, setList] = useState([]);
-  const onCreate = (e) => {
-    const todo = {};
-    setList([...Text, todo]);
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+  const [contextId, setContextId] = useState(0);
+
+  const onCreate = () => {
+    const contextInform = {
+      id: contextId,
+      text,
+    };
+    setList([...list, contextInform]);
+    setText("");
+    setContextId(contextId + 1);
   };
+
   const onChange = (e) => {
     setText(e.target.value);
   };
 
+  const onRemove = (id) => {
+    setList(list.filter((contextInform) => contextInform.id !== id));
+  };
+
   return (
     <>
-      <h1>{setList}</h1>
-      <input value={inputValue} onChange={onChange} />
+      <input value={text} onChange={onChange} />
       <button onClick={onCreate}>추가</button>
+      <ul>
+        {list.map((contextInform) => (
+          <p key={contextInform.id}>
+            <span>{contextInform.text}</span>
+            <button onClick={() => onRemove(contextInform.id)}>삭제</button>
+          </p>
+        ))}
+      </ul>
     </>
   );
 };
+
 export default App;
