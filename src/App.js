22951a0b716321488333c920156ea837a236a799
hyenseochoe <whahoo1@naver.com>
2024-09-03 20:13:46 +0900

    20240903

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ const App = () => {
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
   const [contextId, setContextId] = useState(0);
-
+  const [btn, setBtn] = useState(false);
+  const [transId, setTransId] = useState(0);
   const onCreate = () => {
     const contextInform = {
       id: contextId,
-      text,
+      comment: text,
     };
     setList([...list, contextInform]);
     setText("");
@@ -21,17 +22,38 @@ const App = () => {
   };
 
   const onRemove = (id) => {
-    setList(list.filter((contextInform) => contextInform.id !== id));
+    setList(list.filter((context) => context.id !== id));
+  };
+
+  const onTrue = (id) => {
+    setTransId(id);
+    setBtn(true);
+  };
+
+  const onTransform = () => {
+    {
+      list.map((user) =>
+        user.id == transId ? (user.comment = text) : transId
+      );
+    }
+    setText("");
+    setBtn(false);
   };
 
   return (
     <>
       <input value={text} onChange={onChange} />
-      <button onClick={onCreate}>추가</button>
+
+      {btn == false ? (
+        <button onClick={onCreate}>추가</button>
+      ) : (
+        <button onClick={onTransform}>수정</button>
+      )}
       <ul>
         {list.map((contextInform) => (
           <p key={contextInform.id}>
-            <span>{contextInform.text}</span>
+            <span>{contextInform.comment}</span>
+            <button onClick={() => onTrue(contextInform.id)}>수정</button>
             <button onClick={() => onRemove(contextInform.id)}>삭제</button>
           </p>
         ))}
