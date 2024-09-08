396043a4b5403984757b7c60a18a78014e17ad92
munseongyun <borygashill608@gmail.com>
2024-09-08 22:37:39 +0900

    수정했으면 하는 사항 by 문성윤

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const App = () => {
   const [contextId, setContextId] = useState(0);
   const [btn, setBtn] = useState(false);
   const [transId, setTransId] = useState(0);
+
+  // 현재 complete 배열을 만들어서 완료상태를 관리중인데, 
+  // 이렇게 하면 계속 .includes 메소드를 써야함 .includes는 굉장히 느린 메소드니까,
+  // 객체안에 complete 요소를 하나 만드는게 속도 면에서 유리할거 같음
+  // 그리고 이렇게 배열을 두 개 만들면 json-server로 변경했을때도 데이터베이스 테이블을 2개 만들어야하는 문제가 있음
   const [complete, setComplete] = useState([]);
   const onCreate = () => {
     const contextInform = {
@@ -33,6 +38,10 @@ const App = () => {
   };
 
   const onTransform = () => {
+    // 여기 오류 뜬거 수정하려면 중괄호 없애면 됨
+
+    // map 함수는 배열의 값 하나하나에다가 함수를 적용해서 그 리턴값을 모아서 새로운 배열을 만들때 쓰는거라서 
+    // map보다는 forEach가 좋아보임
     {
       list.map((user) =>
         user.id == transId ? (user.comment = text) : transId
